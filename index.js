a7dfb0a13a1a0c1d2f7593a7e25349d72f5023df
Altoids?! <38045884+CoderSudaWuda@users.noreply.github.com>
2022-01-08 16:25:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
             args[0] = window._server || args[0];
             window._server = args[0];
             window._region = args[0].split('lobby')[1].split('hiss')[0];
+            
+            alert('test: is working!')
 
             return Reflect.construct(Target, args);
         }
